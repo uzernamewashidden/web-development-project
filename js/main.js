038b4fc67f1e15ab6543282db0dd42c21d7de394
uzernamewashidden <usernamewashiddden@gmail.com>
2019-11-05 00:13:52 +0200

    add slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-$(document).ready(function() {
-    $('.topbar__menu-icon').on('click', function() {
-        $('.menu').toggleClass('menu-active');
-    });
+$(document).ready(function () {
+  $('.topbar__menu-icon').on('click', function () {
+    $('.menu').toggleClass('menu-active');
+  });
+
+  $('.team__carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
 });
